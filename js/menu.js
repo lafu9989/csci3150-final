75d7a8dfcd4a2351c8d1c85845c85b5de946700d
Lauren Funston <73963124+lafu9989@users.noreply.github.com>
2023-12-10 10:55:19 -0700

    adding website content

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,11 @@
 function showMenu() {
     var x = document.getElementById("navigation");
+    var header = document.getElementById("header");
     if (x.style.display == "" || x.style.display == "none") {
         x.style.display = "block";
+        header.style.marginBottom = "0px"; 
     } else {
+        header.style.marginBottom = "10px";
         x.style.display = "none";
     }
 }
